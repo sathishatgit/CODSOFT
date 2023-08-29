2e78412f4bd52f10bf2aeda70f4fab7d187eaf08
devsathishatgit <sathishbabudeveloper@gmail.com>
2023-08-29 15:40:11 +0530

    CalCulator First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import subha from "./netaji.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="tributeLayout">
+      <h1>Subhas Chandra Bose</h1>
+      <div className="pic">
+        <img src={subha} alt="Subhas Chandra Bose" />
+      </div>
+      <h4>
+        Subhas Chandra Bose, an epitome of unwavering determination and
+        patriotism, left an indelible mark on India's struggle for independence.
+        Here's a tribute to his remarkable life and contributions:
+      </h4>
+      <div className="Content">
+        <div className="trubutePoints">
+          <h2>About The Legend</h2>
+          👉<b>Fearless Visionary:</b> Subhas Chandra Bose, fondly known as
+          Netaji, envisioned a free India and dedicated his life to realizing
+          this dream.
+        </div>
+        <div className="trubutePoints">
+          👉 <b>Dynamic Leadership:</b> His charismatic leadership galvanized
+          the masses, igniting a fire of resistance against British colonial
+          rule.
+        </div>
+        <div className="trubutePoints">
+          👉<b>Innovative Thinker:</b> Bose's strategic thinking led to the
+          establishment of the Indian National Army (INA), a force aimed at
+          liberating India through collaboration with Japan during World War II.
+        </div>
+        <div className="trubutePoints">
+          👉<b>Unyielding Determination</b>: "Tum mujhe khoon do, main tumhe
+          azaadi dunga" - his resolute call for sacrifice resonated with every
+          Indian, inspiring them to give their all for freedom.
+        </div>
+        <div className="trubutePoints">
+          👉<b>International Advocacy:</b> Bose's international engagements
+          sought global support for India's independence struggle, forming
+          alliances and seeking recognition on international platforms.
+        </div>
+        <div className="trubutePoints">
+          👉<b>Heroic Escape: </b>His daring escape from house arrest in 1941
+          showcased his audacity and unwavering commitment to India's cause.
+          Unity in Diversity: Netaji worked relentlessly to bridge differences,
+          uniting diverse ideologies under a common umbrella for the country's
+          liberation.
+        </div>
+        <div className="trubutePoints">
+          👉<b>Legacy of Courage:</b>His legacy symbolizes courage in the face
+          of adversity, urging us to stand firm against injustice and tyranny.
+        </div>
+        <div className="trubutePoints">
+          👉<b>Eternal Inspiration: </b>Subhas Chandra Bose's legacy continues
+          to inspire us to uphold the values of freedom, justice, and
+          selflessness.
+        </div>
+        <div className="trubutePoints">
+          👉<b>Saluting the Hero:</b> We remember Netaji not just as a freedom
+          fighter, but as a beacon of hope, a visionary who believed in India's
+          potential to rise against all odds.
+        </div>
+        <div className="trubutePoints">
+          Subhas Chandra Bose's life exemplified sacrifice, leadership, and the
+          relentless pursuit of a brighter future for India. His legacy lives
+          on, reminding us that our actions today shape the destiny of our
+          nation.
+        </div>
+      </div>
+      <div>
+        For More Information Check Out Google & Page developed By [
+        <a href="mailto:\sathishbabudeveloper@gmail.com">
+          sathishbabudeveloper@gmail.com
         </a>
-      </header>
+        ]
+      </div>
     </div>
   );
 }
