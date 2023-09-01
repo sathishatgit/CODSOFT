782f034774f3bfd2974b1692bb90df8b1a20db80
devsathishatgit <sathishbabudeveloper@gmail.com>
2023-09-01 15:03:47 +0530

    CalCulator Code
    Calculator Code Cod Soft InternShip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Button, Popover, message } from "antd";
+const math = require("mathjs");
 
 function App() {
+  const [output, setOutput] = useState(0);
+  const btn = ["7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3"];
+  const handleAdd = (val) => {
+    if (output === 0) setOutput(val);
+    else setOutput(output + val);
+  };
+  const handleEquation = () => {
+    try {
+      setOutput(math.evaluate(output));
+    } catch (e) {
+      message.error("Error: " + e.message);
+    }
+  };
+  const content = (
+    <div className="calcOthers">
+      <Button type="primary" onClick={() => setOutput(0)}>
+        Clear Output
+      </Button>
+      <Button type="primary" onClick={() => handleAdd(0)}>
+        Add Zero
+      </Button>
+    </div>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calLayout">
+      <div className="calcOutput">
+        <div className="calcAnswer">{output}</div>
+      </div>
+      <div className="calcButtons">
+        {btn.map((val, index) => (
+          <span
+            className="seperateButtons"
+            key={index}
+            onClick={() => handleAdd(val)}
+          >
+            {val}
+          </span>
+        ))}
+        <span className="seperateButtons" onClick={handleEquation}>
+          =
+        </span>
+      </div>
+      <div className="btn">
+        <Popover content={content} title="Other Options">
+          <Button type="primary">OTHER OPTIONS</Button>
+        </Popover>
+      </div>
     </div>
   );
 }
